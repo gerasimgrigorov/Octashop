20fa208a5c9224dd6892bc6e4c9f055395460776
Gerasim <137315338+geroarmani@users.noreply.github.com>
2023-07-12 13:01:33 +0300

    For the gang

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'templates/navbar.php';
 ?>
 
     <section id="hero">
-      <h4>Trade-in-offer</h4>
+      <h4>Trade-in-offer Gang</h4>
       <h2>Super value deals</h2>
       <h1>On all products</h1>
       <p>Save up to 70% off!</p>
